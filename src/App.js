e308addef040d41fea6a1e499686b50857053bbf
Rondolph S Martin <ronnie@Rondolphs-MacBook-Air.local>
2022-08-04 15:44:48 -0400

    calcultor

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [total,setTotal] = useState(0)
+
+  let digits = [1,2,3,4,5,6,7,8,9,0]
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>Total: {total}</div>
+
+        {digits.map((digit)=>{
+          return <button key={digit} onClick={()=>{setTotal(total+digit)}}>{digit}</button>
+        })}
+<button onClick={()=>{setTotal(0)}}>reset</button> 
+
+{/* <button onClick={()=>{setTotal(total+1)}}>1</button>
+<button onClick={()=>{setTotal(total+2)}}>2</button>
+<button onClick={()=>{setTotal(total+3)}}>3</button>
+<button onClick={()=>{setTotal(total+4)}}>4</button>
+<button onClick={()=>{setTotal(total+5)}}>6</button>
+<button onClick={()=>{setTotal(total+6)}}>7</button>
+<button onClick={()=>{setTotal(total+7)}}>8</button>
+<button onClick={()=>{setTotal(total+8)}}>9</button>
+<button onClick={()=>{setTotal(total+0)}}>0</button>
+*/}
+
+        {/* <button onClick={()=>{
+          setTotal(total+1)
+        }}>+</button> */}
+
       </header>
     </div>
   );
